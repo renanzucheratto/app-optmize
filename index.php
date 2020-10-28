9e95154b42ddb27c75c6e9da229cd871fd9c3237
Lobo Zucheratto <r.lobo.zucheratto@accenture.com>
2020-10-28 10:42:52 -0300

    GA

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 <html lang="en">
 <script src="https://www.googleoptimize.com/optimize.js?id=GTM-PH3L2TF"></script>
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7GZ4PZ1XZM"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-7GZ4PZ1XZM');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste</title>
