06c1fab2c4ff59df3eb5eb97aad9ca3662bf0a4a
Lobo Zucheratto <r.lobo.zucheratto@accenture.com>
2020-10-28 11:34:08 -0300

    configuracao Optimize

--- index.php
+++ index.php
@@ -1,21 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+<script src="https://www.googleoptimize.com/optimize.js?id=OPT-MGQRBRS"></script>
 <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7GZ4PZ1XZM"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-181574280-1"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-7GZ4PZ1XZM');
+    gtag('config', 'UA-181574280-1');
     </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MLGRXMK');</script>
+    <!-- End Google Tag Manager -->
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste</title>
 </head>
-<script src="https://www.googleoptimize.com/optimize.js?id=GTM-PH3L2TF"></script>
 <body>
-    <p>Teste</p>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLGRXMK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    
+    
+    <p>Teste 2</p>
 </body>
 </html>
